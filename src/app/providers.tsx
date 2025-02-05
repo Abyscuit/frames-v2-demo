56f0b7dd98fa0c4e02f58b5a8d7c3cad5fc573bc
Abyscuit <36307949+Abyscuit@users.noreply.github.com>
2025-02-04 22:07:29 -0800

    devop: 🔧 set up providers

---
+++ src/app/providers.tsx
@@ -0,0 +1,16 @@
+'use client';
+
+import dynamic from 'next/dynamic';
+
+const WagmiProvider = dynamic(
+  () => import('@/components/providers/WagmiProvider'),
+  {
+    ssr: false,
+  },
+);
+
+export function Providers({
+  children,
+}: Readonly<{ children: React.ReactNode }>) {
+  return <WagmiProvider>{children}</WagmiProvider>;
+}
