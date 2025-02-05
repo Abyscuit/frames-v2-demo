fbfb7feb1f418a573866d5fb727f981fe663e206
Abyscuit <36307949+Abyscuit@users.noreply.github.com>
2025-02-04 21:31:53 -0800

    chore: 👷 set up prettier

---
+++ .prettierrc.mjs
@@ -0,0 +1,15 @@
+const prettierConfig = {
+  semi: true,
+  singleQuote: true,
+  tabWidth: 2,
+  trailingComma: 'all',
+  bracketSpacing: true,
+  arrowParens: 'always',
+  bracketSameLine: true,
+  jsxSingleQuote: true,
+  singleAttributePerLine: true,
+  printWidth: 80,
+  plugins: ['prettier-plugin-tailwindcss'],
+};
+
+export default prettierConfig;
